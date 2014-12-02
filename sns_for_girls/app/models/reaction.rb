68d6527d80eac6bbfd482f6023fe04988d0c72e5
rubberyuzu <yuzu0125@gmail.com>
2014-12-02 23:02:04 +0900

    created framework... at least no error in rails console...

--- sns_for_girls/app/models/reaction.rb
+++ sns_for_girls/app/models/reaction.rb
@@ -1,5 +1,5 @@
 class Reaction < ActiveRecord::Base
-	attr_accessible :content
+	attr_accessible :content, :user_id
 	belongs_to :user
 	belongs_to :comment
 	has_many :votes, :as => :voteable
--- sns_for_girls/app/models/user.rb
+++ sns_for_girls/app/models/user.rb
@@ -1,9 +1,5 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :token_authenticatable
-  attr_accessible :comments, :followers
-  before_save :ensure_authentication_token
+  attr_accessible :comments, :followings
 end
