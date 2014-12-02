61a7ecbdf3d1fd8ff56f6cc2382ba862c04b60c0
rubberyuzu <yuzu0125@gmail.com>
2014-12-02 15:11:58 +0900

    added a following migration

--- sns_for_girls/db/migrate/20141202060109_create_followings.rb
+++ sns_for_girls/db/migrate/20141202060109_create_followings.rb
@@ -1,7 +1,10 @@
 class CreateFollowings < ActiveRecord::Migration
   def change
     create_table :followings do |t|
-
+    	t.belongs_to :user
+    	t.integer :follower_id 
+    	# follower_id ... the id of the follower
+    	# user ... the person who's followed
       t.timestamps
     end
   end
