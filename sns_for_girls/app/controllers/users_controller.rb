2175b30dfdbf9e8c3f8a190330208cc0ad34b5ad
rubberyuzu <yuzu0125@gmail.com>
2014-12-04 15:48:59 +0900

    created a view for user

--- sns_for_girls/app/controllers/users_controller.rb
+++ sns_for_girls/app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+	def new
+		@new_user = User.new
+	end
+	def create
+		salt = BCrypt::Engine.generate_salt
+		encrypted_password = BCrypt::Engine.hash_secret(params[:user][:password], salt)
+
+		User.create(name: params[:user][:name],
+								email: params[:user][:email],
+								password: encrypted_password)
+	end
 end
--- sns_for_girls/app/models/user.rb
+++ sns_for_girls/app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  attr_accessible :name, :email,:comments, :followings
+  attr_accessible :name, :email,:comments, :password :followings
+  validates :username, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
+  validates :email, :presence => true, :uniqueness => true, :format => EMAIL_REGEX
+  validates :password, :confirmation => true #password_confirmation attr
+  validates_length_of :password, :in => 6..20, :on => :create
 end
